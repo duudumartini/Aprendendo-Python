86c96a38460f277af9eea2e21cedd14e4501fc8b
Eduardo Martini <123970080+duudumartini@users.noreply.github.com>
2023-09-02 19:10:30 -0300

    update

--- Jogo forca/forca.py
+++ Jogo forca/forca.py
@@ -3,16 +3,22 @@ print("************* Bem vindo ao jogo de Forca ***************")
 print("********************************************************")
 
 palavra_secreta = "banana"
+palavra_secreta = palavra_secreta.upper()
+letras_acertadas = ["_", "_", "_", "_", "_", "_", ]
+segredo = "_"
+chances = 5
 enforcou = False
 acertou = False
-posicao = 1
+posicao = 0
 
 while(enforcou == False and acertou == False):
+    print(letras_acertadas)
+    print("Sua palavra possui {} letras".format(len(letras_acertadas)))
     chute = input("Qual a letra? ")
     chute = chute.strip()
-    chute = chute.lower()
-    posicao = 1
+    chute = chute.upper()
+    posicao = 0
     for letra in palavra_secreta:
         if(chute == letra):
-            print("Econtrei a letra {} na posição {}".format(letra, posicao))
+            letras_acertadas[posicao] = letra
         posicao += 1
